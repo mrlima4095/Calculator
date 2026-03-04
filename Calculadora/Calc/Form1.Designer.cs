9d112e8e630ea8ab4ba5aaae09975a17b50f7b12
Mr Lima <felipebr4095@gmail.com>
2026-03-04 13:44:35 -0300

    Background do formulario - Matrix Effect

--- Calculadora/Calc/Form1.Designer.cs
+++ Calculadora/Calc/Form1.Designer.cs
@@ -158,6 +158,7 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
